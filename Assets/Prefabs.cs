c892b8bbca382e83caa3c0734f4c7686e82853b7
tommy <12530425@qq.com>
2022-12-04 23:45:27 +0800

    init

---
+++ Assets/Prefabs.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PrefabAssets",menuName = "Create/Prefab Assets")]
+public class Prefabs : ScriptableObject
+{
+    public List<GameObject> Prefab;
+}
